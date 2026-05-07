65f4f35224206cbdd3d9957b33a865191aa5ec3b
duro92 <pendisu647@gmail.com>
2026-05-07 10:36:36 +0700

    Fix

---
+++ DramaIdProvider/src/main/kotlin/com/dramaid/DramaIdProviderPlugin.kt
@@ -0,0 +1,12 @@
+package com.dramaid
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DramaIdProviderPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DramaIdProvider())
+    }
+}
